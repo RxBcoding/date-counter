6626b3f20727e6d8e850e80d4a59b41600db1fba
Nathaniel McKain <63977243+RxBcoding@users.noreply.github.com>
2024-09-30 10:41:03 -0400

    remove components to stop bad practice of sending parents set state functions down to child component.
    simplify overall application

--- src/App.js
+++ src/App.js
@@ -12,33 +12,8 @@ export default function App() {
 function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
-  const currDate = new Date();
-
-  function handleDateString() {
-    if (count < 0) {
-      currDate.setDate(currDate.getDate() - count);
-      return `${Math.abs(count)} days ago was ${currDate.toDateString()}`;
-    } else if (count > 0) {
-      currDate.setDate(currDate.getDate() + count);
-      return `${count} days from today is ${currDate.toDateString()}`;
-    } else {
-      return `Today is ${currDate.toDateString()}`;
-    }
-  }
-
-  return (
-    <div className="counter">
-      <Step step={step} setStep={setStep} />
-      <Count count={count} setCount={setCount} step={step} />
-      <p>{handleDateString()}</p>
-    </div>
-  );
-}
-
-function Step({ step, setStep }) {
-  function handleStepIncrease() {
-    setStep((currStep) => currStep + 1);
-  }
+  const date = new Date();
+  date.setDate(date.getDate() + count);
 
   function handleStepDecrease() {
     if (step > 1) {
@@ -47,28 +22,27 @@ function Step({ step, setStep }) {
   }
 
   return (
-    <div>
-      <button onClick={handleStepDecrease}>-</button>
-      <span>Step: {step}</span>
-      <button onClick={handleStepIncrease}>+</button>
-    </div>
-  );
-}
-
-function Count({ count, setCount, step }) {
-  function handleCountIncrease() {
-    setCount((currCount) => currCount + step);
-  }
-
-  function handleCountDecrease() {
-    setCount((currCount) => currCount - step);
-  }
-
-  return (
-    <div>
-      <button onClick={handleCountDecrease}>-</button>
-      <span>Count: {count}</span>
-      <button onClick={handleCountIncrease}>+</button>
+    <div className="counter">
+      <div>
+        <button onClick={handleStepDecrease}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((cs) => cs + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((cc) => cc - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((cc) => cc + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
   );
 }
