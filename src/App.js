62acc6d2ea90ecc92ff2327768105957bcaca411
Nathaniel McKain <63977243+RxBcoding@users.noreply.github.com>
2024-09-30 10:17:50 -0400

    initial commit, practice using states in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const currDate = new Date();
+
+  function handleDateString() {
+    if (count < 0) {
+      currDate.setDate(currDate.getDate() - count);
+      return `${Math.abs(count)} days ago was ${currDate.toDateString()}`;
+    } else if (count > 0) {
+      currDate.setDate(currDate.getDate() + count);
+      return `${count} days from today is ${currDate.toDateString()}`;
+    } else {
+      return `Today is ${currDate.toDateString()}`;
+    }
+  }
+
+  return (
+    <div className="counter">
+      <Step step={step} setStep={setStep} />
+      <Count count={count} setCount={setCount} step={step} />
+      <p>{handleDateString()}</p>
+    </div>
+  );
+}
+
+function Step({ step, setStep }) {
+  function handleStepIncrease() {
+    setStep((currStep) => currStep + 1);
+  }
+
+  function handleStepDecrease() {
+    if (step > 1) {
+      setStep((currStep) => currStep - 1);
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={handleStepDecrease}>-</button>
+      <span>Step: {step}</span>
+      <button onClick={handleStepIncrease}>+</button>
+    </div>
+  );
+}
+
+function Count({ count, setCount, step }) {
+  function handleCountIncrease() {
+    setCount((currCount) => currCount + step);
+  }
+
+  function handleCountDecrease() {
+    setCount((currCount) => currCount - step);
+  }
+
+  return (
+    <div>
+      <button onClick={handleCountDecrease}>-</button>
+      <span>Count: {count}</span>
+      <button onClick={handleCountIncrease}>+</button>
+    </div>
+  );
+}
