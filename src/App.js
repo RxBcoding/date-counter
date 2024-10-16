4d4a9df122a6ad2a8f41852f3811c13812b5057b
Nathaniel McKain <63977243+RxBcoding@users.noreply.github.com>
2024-10-16 10:48:44 -0400

    made step a slider, gave count an input text field, and created a reset button

--- src/App.js
+++ src/App.js
@@ -15,22 +15,39 @@ function Counter() {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
-  function handleStepDecrease() {
-    if (step > 1) {
-      setStep((currStep) => currStep - 1);
-    }
+  function handleStep(e) {
+    setStep(Number(e.target.value));
+  }
+
+  function handleReset() {
+    setStep(1);
+    setCount(0);
   }
 
   return (
     <div className="counter">
       <div>
-        <button onClick={handleStepDecrease}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep((cs) => cs + 1)}>+</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          /* 
+          Value will only be editable with the onChange event defined, otherwise value will become essentially read-only
+          if I were to use the defaultValue property, the value can be changed even without an onChange event defined but it would 
+          not update the step state
+          */
+          value={step}
+          onChange={handleStep}
+        />
+        <span>{step}</span>
       </div>
       <div>
         <button onClick={() => setCount((cc) => cc - step)}>-</button>
-        <span>Count: {count}</span>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        ></input>
         <button onClick={() => setCount((cc) => cc + step)}>+</button>
       </div>
       <p>
@@ -43,6 +60,11 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
